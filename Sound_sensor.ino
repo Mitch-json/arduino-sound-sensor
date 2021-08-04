d8a3509993b4397e01c2192fef80e64bf640ff0f
Mitch-json <mitchjaga@gmail.com>
2021-08-04 21:55:15 +0300

    Initial commit

---
+++ Sound_sensor.ino
@@ -0,0 +1,20 @@
+const int ledPin = 13; //pin 13 built-in led 
+const int soundPin = A0; //sound sensor attach to A0 
+int threshold = 820; //Set minimum threshold for LED lit 
+
+void setup() { pinMode(ledPin,OUTPUT);//set pin13 as OUTPUT 
+  Serial.begin(9600); //initialize serial 
+} 
+
+void loop() { 
+  int value = analogRead(soundPin);//read the value of A0 
+  Serial.println(value);//print the value 
+  if(value > threshold) //if the value is greater than 600 
+  { 
+    digitalWrite(ledPin,HIGH);//turn on the led 
+     
+  } else { 
+    digitalWrite(ledPin,LOW);//turn off the led } 
+    
+  }
+}
